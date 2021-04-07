a05a59c51b2bd748418408f72f1ab1f4a50cd589
00010833 <00010833wiut@gmail.com>
2021-04-07 19:10:25 +0500

    minor update

--- app.js
+++ app.js
@@ -1,18 +1,76 @@
+
+// third party libs
 const express = require('express')
 const app = express()
+
+// node libs
+const fs = require('fs')
 const PORT = 8000
 
 app.set('view engine', 'pug')
 
-// http://localhost:8000
+// localhost:8000/static/main.css
+app.use('/static', express.static('public'))
+app.use(express.urlencoded({extended: false}))
+
+// localhost:8000/
 app.get('/', (req, res) => {
-  res.render('home')
+  fs.readFile('./data/todos.json', (err, data) => {
+    if (err) throw err
+
+    const todos = JSON.parse(data)
+
+    res.render('home', { todos: todos })
+  })
 })
 
+app.post('/add', (req, res) => {
+  const formData = req.body 
 
+  if (formData.todo.trim() == '') {
+    fs.readFile('./data/todo.json', (err, data) => {
+      if (err) throw err
+
+      const todos = JSON.parse(data)
+
+      res.render('home', { error: true, todos: todos })
+    })
+  } else {
+    fs.readFile('./data/todos.json', (err, data) => {
+      if (err) throw err
+
+      const todos = JSON.parse(data)
+
+      const todo = {
+        id: id(),
+        description: formData.todo,
+        done: false
+      }
+
+      todos.push(todo)
+
+      fs.writeFile('./data/todos.json', JSON.stringify(todos), (err) => {
+        if (err) throw err
+
+        fs.readFile('./data/todos.json', (err, data) => {
+          if (err) throw err
+
+          const todos = JSON.parse(data)
+
+          res.render('home', { success: true, todos: todos })
+        })
+      })
+    })
+  }
+})
 
 app.listen(PORT, (err) => {
   if (err) throw err
 
-  console.log(`This app is running on port ${ PORT }`)
+  console.log(`This app is running on port http://localhost:${ PORT }`)
 })
+
+
+function id () {
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
